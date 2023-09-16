cfef8b55e7350290c34ca92cf74739c9561bc15a
Seamushroomuser <103343629+Seamushroomuser@users.noreply.github.com>
2023-09-16 17:30:19 +0530

    Add files via upload

---
+++ Table generator.py
@@ -0,0 +1,37 @@
+print("Hello")
+print("My developer is Mr. DGRS Dheemanth")
+print("I am developed in python language")
+print("I am a table generator i can generate tables less than 10000")
+a = int(input("Enter a number, try to keep it less than 10K to avoid answer in exponential form: "))
+A = 1
+B = 2
+C = 3
+D = 4
+E = 5
+F = 6
+G = 7
+H = 8
+I = 9
+J = 10
+K = 11
+L = 12
+M = 13
+N = 14
+O = 15
+print(a, "x", A, " = ", a*A)
+print(a, "x", B, " = ", a*B)
+print(a, "x", C, " = ", a*C)
+print(a, "x", D, " = ", a*D)
+print(a, "x", E, " = ", a*E)
+print(a, "x", F, " = ", a*F)
+print(a, "x", G, " = ", a*G)
+print(a, "x", H, " = ", a*H)
+print(a, "x", I, " = ", a*I)
+print(a, "x", J, " = ", a*J)
+print(a, "x", K, " = ", a*K)
+print(a, "x", L, " = ", a*L)
+print(a, "x", M, " = ", a*M)
+print(a, "x", N, " = ", a*N)
+print(a, "x", O, " = ", a*O)
+print("Thank you for using me :), reload to reuse")
+
